9c98dcce7f1ffd34cfa5e8ccb4e19435a6e704e9
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-03-17 19:34:30 -0600

    directory final

--- chamber/scripts/site.js
+++ chamber/scripts/site.js
@@ -53,3 +53,64 @@ function toggleMenu() {
 document
   .querySelector("#hamburger-button")
   .addEventListener("click", toggleMenu);
+
+async function displayDirectory() {
+  // Retrieve the JSON data
+  const response = await fetch("../data/directory.json");
+
+  // Check if the response is ok
+  if (!response.ok) {
+    console.log("Error retrieving JSON data");
+    return;
+  }
+
+  const data = await response.json();
+
+  // Get the div where the sections will be appended
+  const directoryDiv = document.getElementById("directory");
+
+  // Loop through each business in the JSON data
+  data.businesses.forEach((business) => {
+    // Create a new section element
+    const section = document.createElement("section");
+
+    // Set the inner HTML of the section element
+    section.innerHTML = `
+          <h2>${business.name}</h2>
+          <p>${business.address}</p>
+          <p>${business.phone}</p>
+          <a href="${business.website}">${business.website}</a>
+          <img src="${business.image}" alt="${business.name}"/>
+        `;
+    section.classList.add("panel");
+    section.classList.add("directory-panel");
+    section.setAttribute("id", "directory-panel-id");
+    if (business.membership == "gold") {
+      section.classList.add("gold-membership");
+    }
+
+    // Append the section element to the directory div
+    directoryDiv.appendChild(section);
+  });
+}
+
+// Call the displayDirectory function to display the directory on the page
+const directoryDivExists = document.getElementById("directory");
+if (directoryDivExists) {
+  displayDirectory();
+}
+
+const viewToggle = document.getElementById("view-toggle");
+const directory = document.getElementById("directory");
+
+// Add an event listener to the select element
+viewToggle.addEventListener("change", function () {
+  // Check the value of the select element
+  if (this.value === "list") {
+    // If the value is "list", add the "list" class to the directory element
+    directory.classList.add("list");
+  } else {
+    // Otherwise, remove the "list" class from the directory element
+    directory.classList.remove("list");
+  }
+});
